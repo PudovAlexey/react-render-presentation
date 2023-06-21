934e724cd370eeef0e1fd59b4ebf161ffac144a3
aapudov <aapudov@sberbank.ru>
2023-06-21 09:07:48 +0300

    Поправил верстку

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -58,24 +58,24 @@ export function Chat() {
   }, []);
 
   const handleSearch = () => {
-    setStartSearch(prev => !prev);
+    setStartSearch((prev) => !prev);
   };
 
   return (
     <Root>
       <StarwarsIcon src={iconSrc} />
       <ContentWrapper>
+        <TitleBlock>
+          <StarWarsTitle variant="h2">Персонажи STAR&nbsp;WARS</StarWarsTitle>
+          <Button onClick={onSort}>SORT</Button>
+          <Button onClick={generateMessages}>Сгенерить косарь</Button>
+          <TextField
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <Button onClick={handleSearch}>Поиск</Button>
+        </TitleBlock>
         <AppBox>
-          <TitleBlock>
-            <StarWarsTitle variant="h2">Персонажи STAR&nbsp;WARS</StarWarsTitle>
-            <Button onClick={onSort}>SORT</Button>
-            <Button onClick={generateMessages}>Сгенерить косарь</Button>
-            <TextField
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-            />
-            <Button onClick={handleSearch}>Поиск</Button>
-          </TitleBlock>
           <List>
             {messageIds
               .sort((a, b) => (sortMessages === "asc" ? a - b : b - a))
@@ -138,7 +138,7 @@ const Message = React.memo(
       nameSubstring = "";
     }
 
-    console.log(nameSubstring, 'namesu')
+    console.log(nameSubstring, "namesu");
 
     if (!message) {
       const newMessage = {
@@ -224,19 +224,20 @@ const Message = React.memo(
 const Root = styled(Box)({
   position: "relative",
   height: "100vh",
+  overflow: "hidden",
   width: "100vw",
   backgroundImage: `url(${background})`,
 });
 
-const ContentWrapper = styled(Box)({
+const ContentWrapper = styled(Paper)({
   position: "absolute",
   display: "flex",
-  justifyContent: "space-evenly",
+  flexDirection: "column",
   alignItems: "center",
-  top: "50px",
-  bottom: "50px",
-  left: 0,
-  right: 0,
+  top: "30px",
+  bottom: "120px",
+  left: "50px",
+  right: "50px",
 });
 
 const SendButton = styled(Button)({
@@ -261,16 +262,17 @@ const AppBox = styled(Paper)({
   overflow: "auto",
   height: "100%",
   position: "relative",
+  display: "flex",
+  flexDirection: "column-reverse",
 });
 
 const MessageInputBlockWrapper = styled(Paper)({
   position: "fixed",
   bottom: "10px",
-  right: "6%",
-  width: "85%",
+  right: "0%",
+  width: "94%",
   backgroundColor: "#505050",
-  width: "50%",
-  left: 0,
+  left: "3%",
   right: 0,
   margin: "0 auto",
 });
@@ -282,7 +284,8 @@ const MessageInputBlock = styled(Box)({
   justifyContent: "center",
   alignItems: "center",
   zIndex: 5,
-  width: "85%",
+  right: "0%",
+  width: "90%",
 });
 
 const MessageTypography = styled(Typography)({
